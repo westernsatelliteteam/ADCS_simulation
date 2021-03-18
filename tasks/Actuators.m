7c271718f2093a7a54081c55f5f3541413999f92
Devin Bell <dbell44@uwo.ca>
2021-03-18 18:03:43 -0400

    created ideal magnetorquer model for based on parameters

---
+++ tasks/Actuators.m
@@ -0,0 +1,12 @@
+%% Magnetorquers
+
+vehicle.Actuators.Mag_XY.loops = 1407;
+vehicle.Actuators.Mag_XY.A = 0.0019634954;
+vehicle.Actuators.Mag_XY.length = 0.075;
+vehicle.Actuators.Mag_XY.coreMagFactor = 0;
+
+vehicle.Actuators.Mag_Z.loops = 1407;
+vehicle.Actuators.Mag_Z.A = 0.0019634954;
+vehicle.Actuators.Mag_Z.length = 0.075;
+vehicle.Actuators.Mag_Z.coreMagFactor = 0;
+
